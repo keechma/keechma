0acd6af5374b04229e3611543e0e972bd5f394b3
Mihael Konjevic <konjevic@gmail.com>
2016-01-07 14:39:02 +0100

    WIP. Prototype service manager fn

--- test/ashiba/test/service.cljs
+++ test/ashiba/test/service.cljs
@@ -1,7 +1,7 @@
 (ns ashiba.test.service
   (:require [cljs.test :refer-macros [deftest is async]]
             [ashiba.service :as service]
-            [cljs.core.async :refer [<! >! chan close! sliding-buffer put! alts!]])
+            [cljs.core.async :refer [<! >! chan close! sliding-buffer put! alts! timeout]])
   (:require-macros [cljs.core.async.macros :as m :refer [go alt!]]))
 
 
@@ -42,3 +42,78 @@
 ;;        (h2 chan-3))
 ;;      (put! in-chan :foo)
 ;;      (put! chan-3 "aaaaaaa"))))
+
+(declare on-next-tick-2)
+
+(defn on-next-tick [cb]
+  (.requestAnimationFrame js/window (fn []
+                                      (cb)
+                                      (on-next-tick-2 cb))))
+
+(defn on-next-tick-2 [cb]
+  (on-next-tick cb))
+
+(defn next-tick-chan []
+  (let [c (chan)
+        write-c (fn [] (put! c true))]
+    (on-next-tick write-c)
+    c))
+
+(defn update-state [state fns]
+  (do
+    (.log js/console "UPDATE: " (clojure.core/count fns) (clj->js state))
+    (if-not (empty? fns)
+      (reduce (fn [state cb] (cb state)) state fns)
+      state)))
+
+(defn timeouted [chan data timeout]
+  (do
+    (.log js/console "SETTING UP: " (clj->js data))
+    (.setTimeout js/window (fn []
+                             (put! chan data)) timeout)))
+(defn animation-frame
+  "Return a channel which will close on the nth next animation frame."
+  ([] (animation-frame 1))
+  ([n] (animation-frame n (chan 1)))
+  ([n out]
+     (js/window.requestAnimationFrame
+      (fn [timestamp]
+        (if (= n 1)
+          (do
+            (put! out timestamp)
+            (close! out))
+          (animation-frame (dec n) out))))
+     out))
+
+(deftest chan-timing []
+  (let [ch (chan)
+        state (atom {})
+        updates (atom [])]
+    (do
+      (timeouted ch (fn [state]
+                      (.log js/console "FN 1 CALLED")
+                      (merge state {:fn1 "called"})) 1000)
+      (timeouted ch (fn [state] 
+                      (.log js/console "FN 2 CALLED")
+                      (merge state {:fn2 "called"})) 1500)
+      (async done
+             (go (loop []
+                   (let [new-update-fn (<! ch)]
+                     (swap! updates conj new-update-fn)
+                     (recur))))
+             (go (loop []
+                   (let [updates-fns @updates]
+                     (.log js/console "CALLING UPDATE")
+                     (when-not (empty? updates-fns) 
+                       (.log js/console "UPDATE FNS:" (clj->js @updates))
+                       (reset! state (update-state @state updates-fns))
+                       (reset! updates [])
+                       
+                       (.log js/console "UPDATED STATE: " (clj->js @state)))
+                     (<! (animation-frame 2))
+                     (recur))))
+             (go
+               (<! (timeout 2000))
+               (.log js/console (clj->js @state))
+               (done))))))
+
