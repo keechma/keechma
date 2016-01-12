418bdccb959dd942548d3ec4ec50ddb01b68122e
Mihael Konjevic <konjevic@gmail.com>
2016-01-12 12:30:15 +0100

    Refactpr reify-component test

--- test/ashiba/test/ui_component.cljs
+++ test/ashiba/test/ui_component.cljs
@@ -10,42 +10,39 @@
 
 
 (defn make-container []
-  (let [c (tu/new-container!)
-        unmount (fn []
-                  (tu/unmount! c))]
-    [c unmount]))
-
-(def outer-component 
-  (ui/constructor {:component-deps [:some-component]
-                   :renderer (fn [ctx]
-                               [:div
-                                [:h1 "TEST HERE"]
-                                [(ui/component ctx :some-component)]])}))
-
-(defn inner-render [ctx]
-  (let [on-click (fn [e]
-                   (ui/send-command ctx :inner-command "arg")
-                   (.preventDefault e))]
-    [:button {:on-click on-click} "CLICK ME"]))
-
-(def inner-component
-  (ui/constructor {:renderer inner-render}))
+  (let [c (tu/new-container!)]
+    [c #(tu/unmount! c)]))
 
 (deftest reify-component-test []
   (let [commands-chan (chan)
-        reified-inner (ui/reify-component inner-component {:topic :inner
-                                                           :commands-chan commands-chan})
-        reified-outer (ui/reify-component outer-component {:topic :outer
-                                                           :commands-chan commands-chan
-                                                           :components {:some-component reified-inner}})
+        outer-component (ui/constructor
+                         {:component-deps [:some-component]
+                          :renderer
+                          (fn [ctx]
+                            [:div
+                             [:input {:type :submit
+                                      :on-click #(ui/send-command ctx :outer-command "outer-arg")}]
+                             [(ui/component ctx :some-component)]])})
+        inner-render (fn [ctx]
+                       [:button {:on-click #(ui/send-command ctx :inner-command "inner-arg")}])
+        inner-component (ui/constructor {:renderer inner-render})
+        reified-inner (ui/reify-component inner-component
+                                          {:topic :inner
+                                           :commands-chan commands-chan})
+        reified-outer (ui/reify-component outer-component
+                                          {:topic :outer
+                                           :commands-chan commands-chan
+                                           :components {:some-component reified-inner}})
         [c unmount] (make-container)
         _ (reagent/render-component [reified-outer] c)
-        node (sel1 c [:button])]
+        button-node (sel1 c [:button])
+        submit-node (sel1 c [:input])]
     (async done
            (go
-             (sim/click node nil)
-             (is (= [[:inner :inner-command] "arg"] (<! commands-chan)))
+             (sim/click button-node nil)
+             (is (= [[:inner :inner-command] "inner-arg"] (<! commands-chan)))
+             (sim/click submit-node nil)
+             (is (= [[:outer :outer-command] "outer-arg"] (<! commands-chan)))
              (unmount)
              (done)))))
-
 
