1a6247acd99bdd8111e7310c72b9026e00ff32cc
Mihael Konjevic <konjevic@gmail.com>
2016-10-03 15:46:01 +0200

    New version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject keechma/keechma "0.1.0-SNAPSHOT"
+(defproject keechma/keechma "0.2.0-SNAPSHOT"
   :description "Frontend micro framework for ClojureScript and Reagent"
   :url "http://github.com/keechma/keechma"
   :license {:name "MIT"}
