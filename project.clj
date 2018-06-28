0da9682993028c1468f0202208cd1fa5e9e6fc59
Mihael Konjevic <konjevic@gmail.com>
2018-06-28 16:15:06 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject keechma/keechma "0.3.8"
+(defproject keechma/keechma "0.3.9"
   :description "Frontend micro framework for ClojureScript and Reagent"
   :url "http://github.com/keechma/keechma"
   :license {:name "MIT"}
