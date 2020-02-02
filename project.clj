549918ea5ea8c7f6ac3a97b9f73183e3a793af19
Mihael Konjevic <konjevic@gmail.com>
2020-02-02 20:24:08 +0100

    Start preparing 1.0.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject keechma/keechma "0.3.14"
+(defproject keechma/keechma "1.0.0-SNAPSHOT"
   :description "Frontend micro framework for ClojureScript and Reagent"
   :url "http://github.com/keechma/keechma"
   :license {:name "MIT"}
